27b862f48538567deaa298e1940e4f5182625a5f
MohammadShahabaaz <mohammadshahabaaz@gmail.com>
2024-03-10 19:22:47 +0530

    Version ^18.17.0

---
+++ app/ui/fonts.ts
@@ -0,0 +1,12 @@
+import { Inter,Lusitana } from 'next/font/google';
+ 
+// Define your fonts
+export const inter = Inter({
+    weight: ['400', '700'],
+    subsets: ['latin'] // Specify the subsets for the Inter font
+  });
+  
+  export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'] // Specify the subsets for the Lusitana font
+  });
